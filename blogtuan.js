20f2cd599f1d49a110fea7b74586f039ecb91406
DAU DUC AN <46272881+nhim16@users.noreply.github.com>
2021-08-24 13:23:39 +0700

    Add files via upload

---
+++ blogtuan.js
@@ -0,0 +1,8 @@
+function showComment() {
+var x = document.getElementById(&#39;myDivComments&#39;);
+if (x.style.display === &#39;none&#39;) {
+x.style.display = &#39;block&#39;;
+} else {
+x.style.display = &#39;none&#39;;
+}
+}
